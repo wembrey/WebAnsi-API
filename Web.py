bf8b9f23aae1797cdc3135e5f012a78577b41e6f
Kenneth Helweg Hansen <kenneth@helweghansen.dk>
2018-03-19 13:44:54 +0000

    Added get version

--- Web.py
+++ Web.py
@@ -7,11 +7,13 @@ from ansible.vars.manager import VariableManager
 from ansible.inventory.manager import InventoryManager
 from ansible.executor.playbook_executor import PlaybookExecutor
 from collections import namedtuple
+from bs4 import BeautifulSoup
 
 # Flask app settings
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
+# Credentials of device
 user = 'root'
 password = 'Password'
 
@@ -61,11 +63,42 @@ def execute():
         # Gets the inputs from the forms of '/'
         execute = request.form.to_dict()
         deviceIP = execute['Device']
-        deviceCommand = execute['Command']
 
-        print(devcsl)
+        # SRX REST API connection information
+        headers = {'content-type': 'application/xml'}
+        # authuser = srxuser
+        # authpwd = srxpass
+        # Create the Junos RPC string to add the source IP to the configuration
 
-        return render_template('index.html', deviceList=deviceList, logList=logList)
+        # Assemble the REST call for the SRX
+        # srxaddr = srxmgtip
+        url = "http://" + deviceIP + ":" + "3000" + "/rpc/get-software-information"
+        try:
+            q = requests.get(url,
+                              headers=headers,
+                              auth=(user, password))
+            soup = BeautifulSoup(q.text, "lxml")
+            hostName = soup.find('host-name').string
+            model = soup.find('product-model').string
+            version = soup.find('junos-version').string
+
+
+        except Exception as e:
+            print ("Error: ", e)
+            # Create the logger object
+            jsa_logger = logging.getLogger('jsaLogger')
+            # Send the message as log level WARNING
+            jsa_logger.setLevel(logging.WARNING)
+            # Define the SyslogHandler and assign - log to localhost
+            handler = logging.handlers.SysLogHandler(address=('127.0.0.1', 514))
+            jsa_logger.addHandler(handler)
+            # Write the error message to JSA - will show as a local system message
+            # with a low level category of Stored however event name will be
+            # different depending on the error message
+            jsa_logger.warning("CUSTOM_ACTION_SCRIPT {0}".format(e))
+
+
+        return render_template('index.html', deviceList=deviceList, logList=logList, hostName=hostName, model=model, version=version)
         # return send_from_directory(directory=uploads, filename=filename)
 
 def ansibleRun(deviceHostname, user, password, deviceIP):
@@ -112,10 +145,11 @@ def apiRun(deviceHostname, user, password, deviceIP):
 
     # Connect to SRX and update the configuration with the source IP
     try:
-        q = requests.post(url,
+        requests.post(url,
                       headers=headers,
                       auth=(user, password),
                       data=payload)
+
     except Exception as e:
         print ("Error: ", e)
         # Create the logger object
